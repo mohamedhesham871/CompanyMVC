4abc8157be466bbe8dc7c4464c83b27171ea23df
20100 <mmmelkady23@gmail.com>
2025-04-13 15:04:32 +0200

    add Config

--- MVCCompanyDataAccess/Data/Configration/DepartmentConfig.cs
+++ MVCCompanyDataAccess/Data/Configration/DepartmentConfig.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MVCCompanyDataAccess.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,21 @@ using System.Threading.Tasks;
 
 namespace MVCCompanyDataAccess.Data.Configration
 {
-    internal class DepartmentConfig: IIEntityTypeConfiguration<DepartmentConfig>
+    internal class DepartmentConfig: IEntityTypeConfiguration<Department>
+    {
+       
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.HasKey(d => d.Id);
+            builder.Property(d => d.Id).UseIdentityColumn(10,10);
 
+            builder.Property(d => d.Name).IsRequired().HasMaxLength(30).IsRequired();
+            builder.Property(d => d.Code).IsRequired().HasMaxLength(30).IsRequired();
+            builder.Property(d => d.Description).HasMaxLength(200);
+            builder.Property(d => d.CreatedOn).HasDefaultValueSql("GETDATE");
+            builder.Property(d => d.LastModifiiedOn).HasComputedColumnSql("GETDATE");
+        }
+    }
     {
     }
 }
