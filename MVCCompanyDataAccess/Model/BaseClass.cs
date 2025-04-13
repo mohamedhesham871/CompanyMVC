723ced169540cf6dc8fd687284e546d1bdc62a7b
20100 <mmmelkady23@gmail.com>
2025-04-13 14:25:35 +0200

    Models

---
+++ MVCCompanyDataAccess/Model/BaseClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCCompanyDataAccess.Model
+{
+    internal class BaseClass
+    {
+
+        public int Id { get; set; }//pk
+
+        public int CreatedBy { get; set; }//User ID
+
+        public DateTime? CreatedOn { get; set; }
+
+        public int LastModifiedBy { get; set; }//User ID
+
+        public DateTime? LastModifiiedOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
