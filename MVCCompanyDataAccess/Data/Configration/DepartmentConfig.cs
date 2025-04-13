22670bb93d27c74df7e0676ed7156257f51d7ba6
20100 <mmmelkady23@gmail.com>
2025-04-13 14:43:40 +0200

    Add new layer

---
+++ MVCCompanyDataAccess/Data/Configration/DepartmentConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCCompanyDataAccess.Data.Configration
+{
+    internal class DepartmentConfig: IIEntityTypeConfiguration<DepartmentConfig>
+
+    {
+    }
+}
