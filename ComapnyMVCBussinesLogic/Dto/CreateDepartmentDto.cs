501ca66d1a7fa85a84637425e916653aecb7649c
20100 <mmmelkady23@gmail.com>
2025-04-14 19:27:15 +0200

    add CreateDepartmentDto

---
+++ ComapnyMVCBussinesLogic/Dto/CreateDepartmentDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComapnyMVCBussinesLogic.Dto
+{
+    internal class CreateDepartmentDto
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Code { get; set; } = string.Empty;
+        
+        public string? Description { get; set; } = string.Empty;
+        
+        public DateTime? CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+
+    }
+}
